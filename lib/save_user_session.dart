a4ddaccb47f60cea10cdad3599d07d7d0cda8bd6
Javier Garcia <javierjesusgarciacontreras@gmail.com>
2024-09-02 15:02:36 -0600

    Feat: xapptor_db added

--- lib/save_user_session.dart
+++ lib/save_user_session.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'model/user_session.dart';
+import 'package:xapptor_db/xapptor_db.dart';
 
 save_user_session(String screen_name) async {
   User? current_user = FirebaseAuth.instance.currentUser;
@@ -17,7 +18,7 @@ save_user_session(String screen_name) async {
       date: Timestamp.now(),
     );
 
-    FirebaseFirestore.instance.collection("users").doc(current_user.uid).update({
+    XapptorDB.instance.collection("users").doc(current_user.uid).update({
       "last_sessions": FieldValue.arrayUnion([xapptor_session.to_json()]),
     });
 
@@ -28,7 +29,7 @@ save_user_session(String screen_name) async {
 }
 
 clean_user_sessions(String screen_name, User current_user) async {
-  DocumentSnapshot doc_snap = await FirebaseFirestore.instance.collection("users").doc(current_user.uid).get();
+  DocumentSnapshot doc_snap = await XapptorDB.instance.collection("users").doc(current_user.uid).get();
 
   if (doc_snap.get("last_sessions") != null) {
     UserSessionList last_sessions = UserSessionList.from_snapshot(doc_snap.get("last_sessions"));
@@ -37,7 +38,7 @@ clean_user_sessions(String screen_name, User current_user) async {
       int index = last_sessions.items.length - 10;
       last_sessions.items = last_sessions.items.sublist(index);
 
-      FirebaseFirestore.instance.collection("users").doc(current_user.uid).update({
+      XapptorDB.instance.collection("users").doc(current_user.uid).update({
         "last_sessions": last_sessions.to_json(),
       });
     }
